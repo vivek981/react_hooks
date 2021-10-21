a412cc055a058abf73c71b77a8c2b3734038a22e
Vivek Singh <vivek.singh@VivekS-L.local>
2021-10-21 10:07:00 +0530

    added usecontext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React, {useContext} from 'react';
 import TodoList from './todos/TodoList'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './nav/nav';
 import Home from './Home/home';
 
+
+const themes = {
+  light: {
+    color: 'red',
+  }, 
+  dark: {
+    color: '#000',
+  }
+
+};
+
+const ThemeContext = React.createContext(themes.light);
+
 function App() {
+  const red = useContext(ThemeContext);
   return (
     <Router>
-    <div>
+    <div style={{color: red.color}}>
       <Nav />
       <Switch>
         <Route path='/' exact component={Home} />
